1dc6fb168654e36c0aeb86f11408491795a89187
thanhpham <thanhpham@10.0.0.10>
2014-11-09 00:07:45 -0800

    inital commit all files,tests using PHPUnit_SkeletonGenerator

---
+++ calculator.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of calculator
+ *
+ * @author thanhpham
+ */
+class calculator
+{
+    /**
+     * @assert (0, 0) == 0
+     * @assert (0, 1) == 1
+     * @assert (1, 0) == 1
+     * @assert (1, 1) == 2
+     * @assert (1, 2) == 4
+     */
+
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+
+}
